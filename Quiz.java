17a89a81ab0ffa8952acf02446c7881837a2f94d
victoriaswindleccac <36824272+victoriaswindleccac@users.noreply.github.com>
2018-03-04 21:17:01 -0500

    week 5 quiz

---
+++ Quiz.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz;
+import  java.util.Scanner;
+/**
+ *
+ * @author victoriaswindle
+ */
+public class Quiz {
+
+ public static void main(String[] args){
+        
+          //Program Strings
+        String Title = "--- Beatles Quiz ---";
+        String Question = "what year was john lennon born??";
+        String Correct = "That's correct!";
+        String TooLow = "That's too low... He died in 1980 in New York City...hint hint!! ";
+        String TooHigh = "That's too high! You do know he was the second oldest Beatle right??";
+        String TryAgain = "Try Again!";
+        String Failure = "The answer is ";
+        String Explanation = "October 9th 1940!";
+        String Thanks = "Thanks for trying!";
+        
+        //Integers
+        int Answer = 1940;
+        int AttemptCounter = 0;
+        int UserAnswer;
+        
+        // Scanner for user input
+        Scanner AnswerScanner = new Scanner(System.in);
+        
+        // First output; nothing exciting
+        System.out.println(Title);
+        
+        //While-loop begins; it will end if the user fails three times,
+        //or if the loop is broken.
+        while(AttemptCounter < 3){
+        
+        //User is questioned, and their input is sent through.
+        System.out.println(Question);
+        UserAnswer = AnswerScanner.nextInt();
+        
+        //For a high answer; the user is told that their answer is too high and some humor is involved,
+        //and the Attempt Counter is incremented by one so they can still guess the corrected answer.
+        if(UserAnswer > Answer){
+        System.out.println(TooHigh);
+        
+        //If they can still give another answer the program will let them, 
+        //the user is also asked to try again if they got it wrong.
+        if (AttemptCounter < 2){
+        System.out.println(TryAgain);
+        }
+     
+        AttemptCounter = AttemptCounter + 1;
+        }
+        
+        //For a low Answer; the user is told that their answer is too low qnd yet again, some humor is involved to make it fun!,
+        //aand the Attempt Counter is incremented by one, so they can guess yet again if they didnt get it right already.
+        else if(UserAnswer < Answer){
+        System.out.println(TooLow);
+        
+        //If they can still give  another answer, 
+        //the user is asked to try again.
+        if (AttemptCounter < 2){
+        System.out.println(TryAgain);
+        }
+        
+        AttemptCounter = AttemptCounter + 1;
+        }
+        
+        //For a correct answer, the user is congratulated 
+        //and the while-loop is broken.
+        else if(UserAnswer == Answer){
+        System.out.println(Correct);
+        break;
+        }
+        
+        //If the user has failed three times, 
+        //the user is informed of the correct answer as the loop ends.
+        if(AttemptCounter >= 3){
+        System.out.println(Failure + Answer);
+        }
+    }
+        //However the loop may end, the answer is explained,
+        //and the user is thanked for playing beatles trivia.
+        System.out.println(Explanation + Answer);
+        System.out.println(Thanks);
+    }
+}
